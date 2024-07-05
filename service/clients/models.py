64908507f265ae11ebbce240657cf1c949614569
v0ropaev <workerv0ropaev@yandex.ru>
2024-07-05 21:09:31 +0300

    feat: добавлена модель Client в приложение clients
    - зарегистрированы приложения services и clients в settings.py.
    - добавлена модель Client в приложении clients.
    - выполнена миграция для новой модели в clients.

--- service/clients/models.py
+++ service/clients/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100)
+    company_full_address = models.CharField(max_length=100)
