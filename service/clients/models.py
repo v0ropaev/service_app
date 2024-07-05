36094fd646a7e9e5dcb54e97b9643ec8264bc573
Dmitry Voropaev <144015565+v0ropaev@users.noreply.github.com>
2024-07-05 22:56:56 +0300

    feat: добавлен метод __str__ в модель Client
    - добавлен метод __str__ в модель Client для улучшения отображения объектов модели

--- service/clients/models.py
+++ service/clients/models.py
@@ -6,3 +6,6 @@ class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=100)
     company_full_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'Client {self.pk} | {self.company_name}'
