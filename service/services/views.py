fc2e9a5034fe1217f38255cd22350d81f6d7a228
Dmitry Voropaev <144015565+v0ropaev@users.noreply.github.com>
2024-07-07 22:13:22 +0300

    feat: переопределен метод list в SubscriptionView для добавления общей суммы
    - добавлено вычисление общей суммы подписок с использованием aggregate и Sum
    - результат ответа теперь включает поле total_amount с общей суммой подписок

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch, F
+from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -14,3 +14,14 @@ class SubscriptionView(ReadOnlyModelViewSet):
                  )
     ).annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+
+        response_data = {'result': response.data,
+                         'total_amount': queryset.aggregate(total=Sum('price')).get('total')
+                         }
+        response.data = response_data
+
+        return response
