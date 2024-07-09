bbcc9a3465d6cdc444d97415a273f2facdcbb5f3
Dmitry Voropaev <144015565+v0ropaev@users.noreply.github.com>
2024-07-09 16:40:43 +0300

    feat: оптимизация задачи Celery для расчета цены подписки и добавление celery_singleton
    - добавлен celery_singleton версии 0.3.1 в requirements.txt
    - оптимизирована задача Celery set_price для расчета цены подписки:
      - добавлено использование base=Singleton для предотвращения одновременного выполнения одинаковых задач
      - расчет цены выполнен с использованием аннотаций и фильтрации запросов

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,12 +1,15 @@
 from celery import shared_task
+from celery_singleton import Singleton
+from django.db.models import F
 
 
-@shared_task
+@shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
 
-    subscription = Subscription.objects.get(id=subscription_id)
-    new_price = (subscription.service.full_price -
-                 subscription.service.full_price * subscription.plan.discount_percent / 100)
-    subscription.price = new_price
+    subscription = Subscription.objects.filter(id=subscription_id).annotate(
+        annotated_price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
+    subscription = subscription.first()
+
+    subscription.price = subscription.annotated_price
     subscription.save()
